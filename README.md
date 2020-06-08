ec0067708774bda78cf7e52da323c32e55e0093d
workwithstef <stefan.okolo@outlook.com>
2020-06-08 21:57:02 +0100

    pyodbc work & documentation

---
+++ README.md
@@ -0,0 +1,40 @@
+# Python-to-SQL 
+
+## This repo contains all things pyodbc related
+
+### Procedure to connect Pycharm with SQL database:
+- First step is to 'import pyodbc'
+- Next enter the following string, and save it to a variable
+```
+connect_python = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+
+                                'DATABASE='+database+';UID='+username+';PWD='+password)
+```
+(Inputs; server, database, username & password, are all obtained from SQL Server you wish to link)
+- Define the cursor which you will use to enter SQL commands in Pycharm, using the .cursor() method 
+(usually saved to variable name 'cursor')
+```
+cursor = connect_python.cursor()
+```
+- Use .execute method to execute SQL commands; they should be input as an argument in string-format
+```
+customers = cursor.execute('SELECT * FROM Customers')
+```
+- Successfully run an SQL command in Pycharm!
+
+### Handling SQL received data
+
+- .fetchone() - fetches one entry of received data
+- .fetchall() - fetches all entries of received data
+ 
+##### REMEMBER: cursor remains state -- meaning the data is like a stack of cards,
+          if you take (fetch) one, it removes it from the stack
+
+- customers.fetchone() - will return a pyodbc Row object filled with data
+- Method to efficiently loop through all rows in Customers table
+```
+while True:
+    row = customers.fetchone()
+    print(row)
+    if row is None:
+        break
+```
---
+++ connect_pyodbc.py
@@ -0,0 +1,51 @@
+import pyodbc
+
+server = 'databases2.spartaglobal.academy;'
+database = 'Northwind'
+username = 'SA'
+password = 'Passw0rd2018'
+
+# connects python to SQL database
+connect_python = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+
+                                'DATABASE='+database+';UID='+username+';PWD='+password)
+
+# acts as a cursor in SQL
+cursor = connect_python.cursor()
+
+# executes SQL command
+query_result = cursor.execute('SELECT * FROM Products')
+print(type(query_result))
+
+# REMEMBER: cursor remains state -- meaning the data is like a stack of cards,
+#           if you take (fetch) one, it removes it from the stack.
+
+# fetchone() - fetches one entry in SQL result; removing it from the stack
+# result is received as a 'pyodbc.Row' object, which can be interpreted as a list
+one_entry = query_result.fetchone()
+print(query_result.fetchone())
+print(type(one_entry))
+# fetchall() - fetches all entries in SQL result
+
+all_results = query_result.fetchall()
+# fetchall() results are received as a list
+# lists are iterable.
+print(type(all_results))
+
+# iterating over fetchall list
+#     for data in all_results:
+#         print(data.ProductName, data.UnitPrice)
+
+# fetchall == DANGEROUS. Because all data is received as a single list.
+# when dealing with very large databases, fetchall() method could jam the server
+# Generally, do not use in real life.
+
+customers = cursor.execute('SELECT * FROM Customers')
+
+# continuously loops, re-assigns the fetched result, then prints it, continuously
+# until fetched result is None (~till there are no cards left)
+# Good way to iterate over database
+while True:
+    row = customers.fetchone()
+    print(row)
+    if row is None:
+        break

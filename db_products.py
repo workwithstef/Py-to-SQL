8e64c8f6fd404181fe1a70523445105437c01558
workwithstef <stefan.okolo@outlook.com>
2020-06-10 16:49:08 +0100

    new branch

--- db_products.py
+++ db_products.py
@@ -15,7 +15,24 @@ class ProductTable(MSDBconnection):
             results_list.append(row)
         return results_list
 
+    def add_row(self, product_name, product_price, product_stock_level):
+        # I WANT TO INSERT VALUES TO THE PRODUCT TABLE CORRESPONDING TO THE CURRENT COLUMNS
+        return self.sql_query(f"""
+        INSERT INTO Products
+        (ProductName, UnitPrice, UnitsInStock)
+        VALUES
+        ('{product_name}', '{product_price}', '{product_stock_level}')""")
+
+    def add_column(self, column_name):
+        self.sql_query(f"""
+        ALTER TABLE Products
+        ADD COLUMN {column_name}""")
+
+
+
 
 p_table = ProductTable()
 
-print(p_table.get_by_id(3))
+print(type(p_table.get_by_id(3)))
+
+p_table.add_row("Stefans Special Fried Chicken", 10, 25)

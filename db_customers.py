4c3843037c46789a901fcccc9369a9c799594f31
workwithstef <stefan.okolo@outlook.com>
2020-06-11 13:28:20 +0100

    get by customer complete

--- db_customers.py
+++ db_customers.py
@@ -7,19 +7,19 @@ class Customers(MSDBconnection):
         return self.sql_query('SELECT * FROM Customers WHERE CustomerID=' + str(id)).fetchone()
 
     def get_by_company(self, company_name):
+        name_result_table = self.sql_query(f"SELECT * FROM Customers WHERE CompanyName LIKE '%{company_name}%'")
+        if name_result_table.fetchone() is None:
+            print(f'No results matching "{company_name}".')
         while True:
-            name_result = self.sql_query(f"SELECT * FROM Customers WHERE CompanyName LIKE '%[{company_name}]%'").fetchone()
-            if name_result is None:
+            one_entry = name_result_table.fetchone()
+            if one_entry is None:
                 break
-            # if name_result is None:
-            #     return f'No results matching {company_name}.'
-            else:
-                return name_result
+            print(one_entry)
 
     def get_by_city(self, city):
         city_result_table = self.sql_query(f"SELECT * FROM Customers WHERE City = '{city.capitalize()}'")
         if city_result_table.fetchone() is None:
-            print(f'No results matching {city}.')
+            print(f'No results matching "{city}".')
         while True:
             one_entry = city_result_table.fetchone()
             if one_entry is None:
@@ -50,4 +50,6 @@ class Customers(MSDBconnection):
 client_table = Customers()
 
 client_table.get_by_city('Lonon')
+
+client_table.get_by_company('about')
 

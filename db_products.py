f551c68cda0833ab7fa1aeeea88c2543b83a561d
workwithstef <stefan.okolo@outlook.com>
2020-06-11 14:06:46 +0100

    product class updated

--- db_products.py
+++ db_products.py
@@ -7,16 +7,22 @@ class ProductTable(MSDBconnection):
         return self.sql_query('SELECT * FROM Products WHERE ProductID=' + str(id)).fetchone()
 
     def get_by_name(self, name):
-        return self.sql_query(f"SELECT * FROM Products WHERE ProductName LIKE '%{name}%'").fetchone()
+        name_result_table = self.sql_query(f"SELECT * FROM Products WHERE ProductName LIKE '%{name}%'")
+        if name_result_table.fetchone() is None:
+            print(f'No results matching "{name}".')
+        while True:
+            one_entry = name_result_table.fetchone()
+            if one_entry is None:
+                break
+            print(one_entry)
 
     def get_all(self):
-
+        all_results_table = self.sql_query('SELECT * FROM Products')
         while True:
-            row = self.sql_query('SELECT * FROM Products').fetchone()
-            if row is None:
+            one_entry = all_results_table.fetchone()
+            if one_entry is None:
                 break
-            print(row)
-        # return results_list
+            print(one_entry)
 
     def add_row(self, product_name, product_price, product_stock_level):
         # I WANT TO INSERT VALUES TO THE PRODUCT TABLE CORRESPONDING TO THE CURRENT COLUMNS
@@ -30,36 +36,11 @@ class ProductTable(MSDBconnection):
         self.sql_query(f"""
         ALTER TABLE Products
         ADD COLUMN {column_name}""")
-
-
-
-    def add_row(self, product_name, product_price, product_stock_level):
-        # I WANT TO INSERT VALUES TO THE PRODUCT TABLE CORRESPONDING TO THE CURRENT COLUMNS
-        return self.sql_query(f"""
-        INSERT INTO Products
-        (ProductName, UnitPrice, UnitsInStock)
-        VALUES
-        ('{product_name}', '{product_price}', '{product_stock_level}')""")
-
-    def add_column(self, column_name):
-        self.sql_query(f"""
-        ALTER TABLE Products
-        ADD COLUMN {column_name}""")
-
-
 
 
 p_table = ProductTable()
 
+# p_table.add_row("Stefans Special Fried Chicken", 10, 25)
 
-print(type(p_table.get_by_id(3)))
-
-p_table.add_row("Stefans Special Fried Chicken", 10, 25)
-
-# print(type(p_table.get_by_id(3)))
-
-p_table.add_row("Stefans Special Fried Chicken", 10, 25)
-
-# print(p_table.get_by_name('Stefan'))
-p_table.get_all()
+p_table.get_by_name('chef')
 
